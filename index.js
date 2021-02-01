cdea723efec7580b5c892daca36072245ed44cfa
Vlad <vladhopilo@gmail.com>
2021-02-01 22:59:03 +0200

    upd

--- index.js
+++ index.js
@@ -10,21 +10,46 @@ let current = 0;
 
 nextBtnRef.addEventListener("click", () => {
   let contain = null;
-  console.log(current);
+  //   console.log(current);
 
   //дальше расматриваем все наши боксы отдельно
   for (let i = 0; i < allBoxRef.length; i += 1) {
-    console.log(current);
+    // console.log(current);
     allBoxRef[i].classList.add(`${i}`);
 
     //скрываем первый элемент, после бокса с классом is-hidden
+    //     if (i === current) {
+    //       allBoxRef[i].classList.add("is-hidden");
+    //       console.log(allBoxRef[i]);
+    //       contain = allBoxRef[i].classList.contains("is-hidden");
+
+    //       console.log(contain);
+    //       current += 1;
+    //       return;
+    //     }
+    //   }
+    // });
+
     if (i === current) {
-      allBoxRef[i].classList.add("is-hidden");
-      console.log(allBoxRef[i]);
-      contain = allBoxRef[i].classList.contains("is-hidden");
-      console.log(contain);
+      setTimeout(() => {
+        allBoxRef[i].classList.add("is-hidden");
+        // console.log(allBoxRef[i]);
+        contain = allBoxRef[i].classList.contains("is-hidden");
+      }, 300);
+
+      swiperWrapperRef.classList.add("is-visible");
+
+      //   console.log(contain);
       current += 1;
       return;
     }
   }
+});
+
+prevBtnRef.addEventListener("click", () => {
+  const remove = () => {
+    swiperWrapperRef.classList.remove("is-visible");
+  };
+
+  remove();
 });
