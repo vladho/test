d37a2077ae92579009bafc5f80c2299edbb032b7
Vlad <vladhopilo@gmail.com>
2021-02-02 22:38:50 +0200

    addindicator

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const nextBtnRef = document.querySelector(".swiper-button-next");
 const allBoxPseudoArrayRef = document.querySelectorAll(".swiper-slide");
 
 const allBoxArrayRef = [...allBoxPseudoArrayRef];
+const allIndicatorRef = document.querySelectorAll(".indcator-box");
+const allIndicator = [...allIndicatorRef];
 
 const swiperWrapperRef = document.querySelector(".swiper-wrapper");
 
-let contain = null;
+let calc = 1;
 
 const widthOfOneBlock = 150;
 const findMaxEl = (allBoxArrayRef.length - 2) * widthOfOneBlock;
@@ -20,6 +22,10 @@ prevBtnRef.addEventListener("click", () => {
   }
 
   transformForX += 150;
+  calc -= 1;
+
+  allIndicator[calc].classList.remove("is-active");
+  allIndicator[calc - 1].classList.add("is-active");
 
   swiperWrapperRef.style.transform = `translate3d(${
     transformForX + "px"
@@ -30,6 +36,10 @@ nextBtnRef.addEventListener("click", () => {
   if (transformForX < -findMaxEl) {
     return;
   }
+  calc += 1;
+
+  allIndicator[calc - 2].classList.remove("is-active");
+  allIndicator[calc - 1].classList.add("is-active");
 
   transformForX -= 150;
   swiperWrapperRef.style.transform = `translate3d(${
